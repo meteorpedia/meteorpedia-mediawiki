f6678fee7a0e0574d836eb71f6b92e92955249aa
Gadi Cohen <dragon@wastelands.net>
2013-07-10 09:03:32 +0300

    added some comments

--- package.js
+++ package.js
@@ -9,6 +9,7 @@ Package.on_use(function (api) {
 	// Extensions MUST include this line if they include .html files
 	api.use('templating', 'client');
 
+	// client files (js, html)
 	api.add_files(['lib/highlight.js', 'lib/mediawiki.css', 'lib/categories/categories.css',
 		'lib/categories/categories.html', 'lib/special/special.html',
 		'lib/tables/tables.css', 'lib/links/links.html', 'lib/links/links.css',
@@ -17,11 +18,13 @@ Package.on_use(function (api) {
 		'lib/tags.js'],
 	'client');
 
+	// client files (images)
 	api.add_files([
 		'lib/links/img/external-link-ltr-icon.png',
 		'lib/links/img/lock-icon.png'
 	], 'client');
 
+	// common files (js on server+client)
     api.add_files(['lib/mediawiki.js', 'lib/categories/categories.js', 'lib/links/links.js',
     	'lib/cite.js', 'lib/parserFuncs.js', 'lib/special/special.js', 'lib/tables/tables.js',
     	'lib/preview/preview.js', 'lib/allpages/allpages.js', 'lib/recentchanges/recentchanges.js',
