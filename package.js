8c8453c6bd25082b92da82f2c1015c0306f8e915
Gadi Cohen <dragon@wastelands.net>
2013-07-11 23:52:54 +0300

    search!  infinite scrolling coming soon

--- package.js
+++ package.js
@@ -9,13 +9,13 @@ Package.on_use(function (api) {
 	// Extensions MUST include this line if they include .html files
 	api.use('templating', 'client');
 
-	// client files (js, html)
+	// client files (js, html, css)
 	api.add_files(['lib/highlight.js', 'lib/mediawiki.css', 'lib/categories/categories.css',
 		'lib/categories/categories.html', 'lib/special/special.html',
 		'lib/tables/tables.css', 'lib/links/links.html', 'lib/links/links.css',
 		'lib/preview/preview.css', 'lib/toc.js', 'lib/allpages/allpages.html',
 		'lib/recentchanges/recentchanges.html', 'lib/mediawiki.html',
-		'lib/tags.js'],
+		'lib/tags.js', 'lib/search/search.html', 'lib/search/search.css'],
 	'client');
 
 	// client files (images)
@@ -28,6 +28,6 @@ Package.on_use(function (api) {
     api.add_files(['lib/mediawiki.js', 'lib/categories/categories.js', 'lib/links/links.js',
     	'lib/cite.js', 'lib/parserFuncs.js', 'lib/special/special.js', 'lib/tables/tables.js',
     	'lib/preview/preview.js', 'lib/allpages/allpages.js', 'lib/recentchanges/recentchanges.js',
-    	'lib/namespaces.js'],
+    	'lib/namespaces.js', 'lib/search/search.js'],
     ['client', 'server']);
 });
